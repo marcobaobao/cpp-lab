18d925e9944eda278d079245f8d3726046bb3df9
marcobaobao <marco.piovanello@gmail.com>
2021-10-24 18:24:53 +0200

    lab 1 - book finale

--- lab-1/book_isbn/bookTester.cpp
+++ lab-1/book_isbn/bookTester.cpp
@@ -9,15 +9,12 @@ int main()
     cout << b1;
 
     b1.checkOut();
-
     cout << b1;
 
     Book b2("12-3-2222-X", "another book", "mine", Date(29, 3, 2018), true);
-
     cout << b2;
 
     b2.checkIn();
-
     cout << b2;
 
     cout << "> " << (b1 == b2 ? "ISBN matches" : "ISBN doesn't matches") << "\n";
